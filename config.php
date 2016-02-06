567c4605d553b7d7313c25602011f95a745e3083
TAB_mk <tabmk999@gmail.com>
2016-02-07 00:35:13 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$chat_id        = ""; // Chat id. Take id from bot page!
+$access_token   = ""; // Bot page access token. Take http://vk.cc/4KeIFn (copy part in address bar between "access_token=" and "&expires_in")
+$client_id      = ""; // Create app here - https://vk.com/dev/ and take Application ID in settings
+$client_secret  = ""; // Secure key in settings too
+$redirect_uri   = "http://your-site.com/conversation/index.php"; // Index script page
+$moder_id       = array("116384435", "1", "123123123"); // array with moderators ids
+$reason_message = true; // Switching this to "false" if you don't want use "reason message"
+$bot_prefix     = "(ChatMod)"; // Prefix for "reason message"
+?>
