6c756f59cf9d5943206b5a2c33128360d8f5e357
TAB_mk <tabmk999@gmail.com>
2016-05-17 21:36:37 +0300

    added error check

--- remove.php
+++ remove.php
@@ -1,6 +1,5 @@
 <?php
 	include 'config.php';
-
 	$kickid = htmlspecialchars($_POST['kickid']);
 	$reason = htmlspecialchars($_POST['reason']);
 	
@@ -8,9 +7,16 @@
 	$reason = str_replace(" ", "%20", $reason);
 	
 	if($reason_message == true){
-	file_get_contents("https://api.vk.com/method/messages.send?access_token={$access_token}&chat_id={$chat_id}&message={$reason}", true);
+		file_get_contents("https://api.vk.com/method/messages.send?access_token={$access_token}&chat_id={$chat_id}&message={$reason}", true);
 	}
-	file_get_contents("https://api.vk.com/method/messages.removeChatUser?access_token={$access_token}&chat_id={$chat_id}&user_id={$kickid}", true);
+
+	$kick = file_get_contents("https://api.vk.com/method/messages.removeChatUser?access_token={$access_token}&chat_id={$chat_id}&user_id={$kickid}", true);
+	$kick = json_decode($kick);
+	$err = $kick->error->error_code;
 	
-	header("Location: https://vk.com/im");
+	if($err == null){
+		header("Location: https://vk.com/im");
+	}else{
+		echo "<b>!</b> Error while kicking user. Please, try again.<br><b>Eror info</b>:<br>Error code - <i>{$kick->error->error_code}</i><br>Error message - <i>{$kick->error->error_msg}</i>" ;
+	}
 ?>
